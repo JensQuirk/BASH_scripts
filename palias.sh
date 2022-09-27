84a57cebb4892ea1f3c73100bae1a190bdf37fa5
Jens Quirk <jens.quirk@gmail.com>
2022-09-27 19:05:18 +0200

    Update palias.sh

--- palias.sh
+++ palias.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#Make persistent aliases quickly
-#Format: $1=name $2=command
-#example: sh palias.sh name command
 
-text="alias $1='$2'"
-echo $text >> ~/.bashrc
-source ~/.bashrc
+#Make persistent aliases quickly
+
+#Format: palias [name] [command]
+#Create persistent alias for palias: sh [loaction of palias.sh file]/palias.sh -s
+#Remember to execute .bashrc to use aliases in current shell session.(source ~/.bashrc)
+
+if [ "$1" = "-s" ];
+then
+    echo "alias palias='sh $2'" >> ~/.bashrc
+elif [ "$1" != '' ] && [ "$2" != '' ];
+then
+    text="alias $1='$2'"
+    echo $text >> ~/.bashrc
+else
+    echo 'Invalid parameters. Please type: palias [alias name] [commands]'
+fi
